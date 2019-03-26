d1cf7c3d559f011887a1a9e3dfc95adfff4e25ac
zgw95 <48548914+zgw95@users.noreply.github.com>
2019-03-26 21:46:23 +0800

    Add files via upload

---
+++ bottom.js
@@ -0,0 +1,43 @@
+$("#bottom").css({
+				"width":"100%",
+				"height":"50px",
+				"display":"flex",
+			})
+
+$(".bottom-menu").css({
+				"width":"75px",
+				"height":"100px",
+//				"border":"1px solid black",
+				"display":"flex",
+				"flex-direction":"column",
+				"align-items":"center",	
+})
+$("#index").css({
+				"width":"35px",
+				"height":"25px",
+				"margin-top":"5px",
+})
+$("#fenlei").css({
+				"width":"24px",
+				"height":"24px",
+				"margin-top":"6px",
+})
+$("#me").css({
+				"width":"75px",
+				"height":"30px",
+				"margin-top":"12px",
+})
+$("#gouwuche").css({
+				"width":"30px",
+				"height":"30px",
+				"margin-top":"1px",
+})
+$("#gengduo").css({
+				"width":"30px",
+				"height":"30px",
+				"margin-top":"1px",
+})
+
+$(".menu-word").css({
+				"font-size":"12px",
+})
