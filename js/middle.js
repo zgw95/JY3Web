80900ca21d949cd2c4b2444b056fc299eacbf6dd
zgw95 <48548914+zgw95@users.noreply.github.com>
2019-03-28 20:11:57 +0800

    Add files via upload

---
+++ js/middle.js
@@ -0,0 +1,45 @@
+//中间分为四个部分
+$("#middle").append("<div id='second'></div>"+	  	
+				"<div id='third'></div>"+
+				"<div id='forth'></div>")
+//第一部分
+
+
+//第二部分
+$("#second").append("<div></div> <div></div>")
+$("#second div").addClass("tubiaokuai")
+
+$("#second div:first").append("<div><img src='img/logo/外星人.svg'></div>"+
+						"<div><img id='hwlg' src='img/logo/华为.svg'></div>"+ 
+						"<div><img src='img/logo/苹果.svg'></div>")
+
+$("#second div:last").append("<div><img src='img/logo/小米.svg'></div>"+
+						"<div><img src='img/logo/联想.svg'></div>"+ 
+						"<div><img src='img/logo/微软.svg'></div>")
+$("#second img:eq(1)").addClass("img60px")
+$("#second img:eq(2)").addClass("img60px")
+$("#second img:eq(3)").addClass("img60px")
+$("#second img:eq(4)").addClass("img60px")
+
+$("#second img:eq(0)").addClass("img50px")
+$("#second img:eq(5)").addClass("img50px")
+
+//第三部分 热卖单品
+$("#third").append("<div id='third-top'></div> <div id='third-bottom'></div>")
+$("#third-top").append("热卖单品")
+$("#third-bottom").append("<a href='华为mate20.html'><div></div></a> <div></div> <div></div> <div></div>")
+
+$("#third-bottom div:eq(0)").append("<img src='img/商品/华为.jpg'><p>华为Mate20</p><p>￥5499</p>")
+$("#third-bottom div:eq(1)").append("<img src='img/商品/苹果.jpg'><p>iphoneXR</p><p>￥5949</p>")
+$("#third-bottom div:eq(2)").append("<img src='img/商品/小米9.jpg'><p>小米9</p><p>￥2999</p>")
+$("#third-bottom div:eq(3)").append("<img src='img/商品/微软电脑.jpg'><p>Surface Book 2</p><p>￥22888</p>")
+
+//第四部分 猜你喜欢
+$("#forth").append("<div id='forth-top'></div> <div id='forth-bottom'></div>")
+$("#forth-top").append("猜你喜欢")
+$("#forth-bottom").append("<div></div> <div></div> <div></div> <div></div>")
+
+$("#forth-bottom div:eq(0)").append("<img src='img/商品/iwatch.jpg'><p>Apple Watch</p><p>￥3499</p>")
+$("#forth-bottom div:eq(1)").append("<img src='img/商品/佳能相机.jpg'><p>佳能相机</p><p>￥5098</p>")
+$("#forth-bottom div:eq(2)").append("<img src='img/商品/耳机.jpg'><p>Beats耳机</p><p>￥899</p>")
+$("#forth-bottom div:eq(3)").append("<img src='img/商品/戴森吹风机.jpg'><p>戴森吹风机</p><p>￥2990</p>")
---
+++ js/new.js
@@ -0,0 +1,8 @@
+$("#new-1").append("<a href='登录界面.html'><img src='img/menu/左箭头.svg'/></a> <p>注册</p>")
+$("#new-2").append("<div>国家/地区</div> <select></select>")
+$("#new-2 select").append("<option>中国大陆（+86)</option>"+
+			"<option>美国（+1)</option>"+
+			"<option>德国（+49)</option>"+
+			"<option>俄罗斯（+7)</option>")
+$("#new-3").append("<div>手机号</div> <input type='text'></input>")
+$("#new-4").append("<button>点击完成验证</button>")
