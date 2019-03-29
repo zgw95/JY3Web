3363bf6f0075594fd3b85f3a2df76304aca3704a
zgw95 <48548914+zgw95@users.noreply.github.com>
2019-03-29 16:40:15 +0800

    Add files via upload

---
+++ 电商20190329/js/mate20购买新.js
@@ -0,0 +1,12 @@
+$("#head").append("<img id='huawei' src='img/商品/华为.jpg'/> <span id='jiage'>￥5489</span> <span id='kucun'>库存充足</span> <img id='cancel'src='img/logo/取消.svg'/>")
+$("#body").append("<div id='content'></div> <div id='count_choose'></div> <div id='determine'>确定</div>")
+$("#content").append("<div id='yanse'>颜色</div> <div id='yansefenlei'></div>" +
+	"<div id='banben'>版本</div> <div id='banbenfenlei'></div>" +
+	"<div id='neicun'>内存</div> <div id='neicunfenlei'></div>")
+$("#yansefenlei").append("<span>亮黑色</span> <span>极光色</span> <span>翡冷翠</span>"+
+	"<span>宝石蓝</span> <span>樱粉金</span> <span>馥蕾红</span> <span>璨星蓝</span>")
+$("#banbenfenlei").append("<span>标准版</span><span>碎屏险套装版</span><span>无忧套装版</span>")
+$("#neicunfenlei").append("<span>8G+128G</span><span>6G+128G</span><span>8G+256G</span>")
+$("#count_choose").append("<div id='count'>数量</div> <div id='num'></div>")
+$("#num").append("<div id='minus1'> - </div> <div id='text'> 1 </div> <div id='plus1'> + </div>")
+
