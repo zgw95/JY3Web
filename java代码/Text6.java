9dfc132bf3185ea98a7d0f6119b36742586b7af3
zgw95 <48548914+zgw95@users.noreply.github.com>
2019-04-03 18:59:01 +0800

    Add files via upload

---
+++ java代码/Text6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Text6
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		while(true)
+		{
+			System.out.println("您输入的数是");
+			int num = scanner.nextInt();
+			if(num == -1)
+			{
+				break;
+			}
+		}
+	}
+}
